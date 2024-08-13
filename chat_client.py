51174b2c95f9c2eee5d4e3e235ba9b58db8f02a8
ObeOne-KMITL <73372869+ObeOne-KMITL@users.noreply.github.com>
2024-08-13 10:25:48 +0700

    Update chat_client.py

--- chat_client.py
+++ chat_client.py
@@ -1,23 +1,28 @@
-
 import asyncio
 import websockets
 
-async def send_message(websocket):
-        while True:
-                message = input("Enter Message : ")
-                await websocket.send(message)
-
-async def recieve_message(websocket):
-        async for message in websocket:
-                print(f"Recieve : {message}")
+async def input_in_thread():
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(None, input,"Enter Message : ")
 
 async def main():
         uri = "ws://192.168.188.143:8765"
         async with websockets.connect(uri) as websocket:
-                await asyncio.gather(
-                        send_message(websocket),
-                        recieve_message(websocket)
-                )
+                print("Connect to Server")
+
+                async def send_message():
+                        while True:
+                                message = await input_in_thread()
+                                await websocket.send(message)
+                async def recieve_message():
+                        while True:
+                                try:
+                                        message = await websocket.recv()
+                                        print(f"Recieve : {message}")
+                                except websockets.exceptions.ConnectionClosed:
+                                        print("Conection Closed")
+                                        break
+                await asyncio.gather(send_message(), recieve_message())
 
 if __name__== "__main__":
         asyncio.run(main())
